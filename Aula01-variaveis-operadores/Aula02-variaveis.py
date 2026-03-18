5bbd8a8ae551ab8991c7a5db8b89a2e6e463847e
DavidCardoso22 <12meses.4semanas@gmail.com>
2026-03-18 20:49:20 -0300

    aula 2 variaveis

---
+++ Aula01-variaveis-operadores/Aula02-variaveis.py
@@ -0,0 +1,26 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7 + "4"") # CONCATENANDO STRINGS
+
+# Comentario de 1 linha
+'''
+    Comentarios de 
+    mútiplas
+    limhas
+'''
+
+# VARIÁVEIS
+nome = "Alexande" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print("Oiii", nome, idade, peso)
+print(f"Oiii {nome}!!!")
+
+# INPUT - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite seu nome: ") # str
+idade = int(input("Digite sua idade:"))
+peso = float(input("Digite seu peso: "))
+
---
+++ Aula01-variaveis-operadores/Desafio03.py
@@ -0,0 +1,4 @@
+dia = int(input("Digite o dia: "))
+mes = int(input("Digite o mes: "))
+ano = int(input("Digite o ano: "))
+print(f"{dia}/{mes}/{ano}")
